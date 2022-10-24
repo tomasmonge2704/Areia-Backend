cefacb5f942e4b332e557eeb5953abe6e9ae85b8
Tomas Monge <tomasmonge@Tomass-MBP.fibertel.com.ar>
2022-10-24 00:48:12 -0300

    fix

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ if (cluster.isMaster && config.CLUSTER == "on" ){
         cluster.fork();
     }
     cluster.on('exit',(worker,code,signal)=>{
-        console.log(`worker ${worker.process.pid} died`)
+        console.log(`worker ${worker.process.pid} diedd`)
         cluster.fork()
     })
 
